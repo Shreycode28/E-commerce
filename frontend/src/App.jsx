5e46d5b3af704d877ea3f336644e89ef0028031c
Shreycode28 <shreypatel282006@gmail.com>
2025-01-30 13:15:28 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignupPage, Home } from "./Routes";
+import { LoginPage ,SignupPage,Home ,CreateProduct} from "./Routes";
 
-import './App.css'
+import "./App.css";
 
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignupPage/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/home' element = {<Home/>}/>
+        <Route path='/product' element = {<CreateProduct/>}/>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
 export default App;
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,10 @@
-import LoginPage from './pages/Login'
+import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
-import Home from './pages/Home';
+import Home from './pages/Home'
+import CreateProduct from "./pages/createProduct";
 
-export {LoginPage,SignupPage,Home};
+
+export { LoginPage };
+export{SignupPage};
+export{Home};
+export{CreateProduct}
