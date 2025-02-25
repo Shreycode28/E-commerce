8e97df6755f528c4cff959ad8950efd7d311f3f9
Shreycode28 <shrey.patel.s73@kalvium.community>
2025-02-25 16:22:16 +0530

    M15 done

---
+++ frontend/src/pages/cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/auth/nav'
+
+const Cart=()=>{
+    return(
+        <>
+        <NavBar/>
+        <h1>Cart</h1>
+        </>
+    )
+    
+}
+
+export default Cart;
+
